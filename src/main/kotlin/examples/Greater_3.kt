02b2d5c429285018464b7933a4eb551766d879a3
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-06-09 21:44:32 +0530

    Create Greater_3.kt

---
+++ src/main/kotlin/examples/Greater_3.kt
@@ -0,0 +1,29 @@
+package examples
+
+fun main() {
+    println("///Greater Number Program for 3 Numbers///")
+    println("\nEnter First Number: ")
+    val firstNumber: Int = Integer.valueOf(readLine())
+    println("\nEnter Second Number: ")
+    val secondNumber: Int = Integer.valueOf(readLine())
+    println("\nEnter Third Number: ")
+    val thirdNumber: Int = Integer.valueOf(readLine())
+    var greatestNumber: Int = 0
+    if (firstNumber > secondNumber) {
+        if (firstNumber > thirdNumber) {
+            greatestNumber = firstNumber
+        }
+        else {
+            greatestNumber = thirdNumber
+        }
+    }
+    else {
+        if (secondNumber > thirdNumber) {
+            greatestNumber = secondNumber
+        }
+        else {
+            greatestNumber = thirdNumber
+        }
+    }
+    println("\nGreatest Number among $firstNumber, $secondNumber, and $thirdNumber is = $greatestNumber")
+}
