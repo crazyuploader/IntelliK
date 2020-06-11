c67d95ffe74e09fc8c4dd076278c3a9490857f3c
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-06-11 23:37:28 +0530

    Update Average.kt

--- src/main/kotlin/examples/Average.kt
+++ src/main/kotlin/examples/Average.kt
@@ -9,7 +9,7 @@ fun main() {
     println("///Program to get average of n Entered Number")
     println("\nEnter of numbers to get Average: ")
     val size: Int = Integer.valueOf(readLine())
-    var numbers = ArrayList<Int>()
+    val numbers = ArrayList<Int>()
     var i = 1
     while (i <= size) {
         println("\nEnter $i element: ")
