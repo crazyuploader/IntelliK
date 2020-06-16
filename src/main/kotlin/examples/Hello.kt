b4f63664cf60c907d207748c4e4b1a6c87aa7db4
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-06-16 20:25:01 +0530

    Create Hello.kt

---
+++ src/main/kotlin/examples/Hello.kt
@@ -0,0 +1,10 @@
+package examples
+
+/**
+ * Function which does nothing than to print "Hello World!"
+ */
+
+fun main() {
+    println("///First Kotlin Program///")
+    println("\nHello World!")
+}
